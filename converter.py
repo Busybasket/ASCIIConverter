01ebb92a8f3027cfa531d6024f2fd81b9e5d0ffe
Busybasket <86014728+Busybasket@users.noreply.github.com>
2021-07-03 14:43:50 +0300

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,162 +1,105 @@
-
-def cevirici(deger):
-    
-    if(deger == 'a'):
-        deger = "97"
-    elif(deger == "b"):
-        deger = "98"
-    elif(deger == "c"):
-        deger = "99"
-    elif(deger == "d"):
-        deger = "100"
-    elif(deger == "e"):
-        deger = "101"
-    elif(deger == "f"):
-        deger = "102"
-    elif(deger == "g"):
-        cıktı = "103"
-    elif(deger == "h"):
-        cıktı = "104"
-    elif(deger == "i"):
-        cıktı = "105"
-    elif(deger == "j"):
-        cıktı = "106"
-    elif(deger == "k"):
-        cıktı = "107"
-    elif(deger == "l"):
-        cıktı = "108"
-    elif(deger == "m"):
-        cıktı = "109"
-    elif(deger == "n"):
-        cıktı = "110"
-    elif(deger == "o"):
-        cıktı = "111"
-    elif(deger == "p"):
-        cıktı = "112"
-    elif(deger == "r"):
-        cıktı = "113"
-    elif(deger == "s"):
-        cıktı = "114"
-    elif(deger == "t"):
-        cıktı = "115"
-    elif(deger == "u"):
-        cıktı = "116"
-    elif(deger == "v"):
-        cıktı = "117"
-    elif(deger == "w"):
-        cıktı = "118"
-    elif(deger == "x"):
-        cıktı = "120"
-    elif(deger == "y"):
-        cıktı = "121"
-    elif(deger == "z"):
-        cıktı = "122"
-    elif(deger == "0"):
-        cıktı = "48"
-    elif(deger == "1"):
-        cıktı = "49"
-    elif(deger == "2"):
-        cıktı = "50"
-    elif(deger == "3"):
-        cıktı = "51"
-    elif(deger == "4"):
-        cıktı = "52"
-    elif(deger == "5"):
-        cıktı = "53"
-    elif(deger == "6"):
-        cıktı = "54"
-    elif(deger == "7"):
-        cıktı = "55"
-    elif(deger == "8"):
-        cıktı = "56"
-    elif(deger == "9"):
-        cıktı = "57"
-    return deger
-
-print("------------------------\nASCII Dec-Char converter\n------------------------\nSonuçlar bilgiler.txt ye kaydedilecek\n------------------------")
-print("chardan - dece çevirmek için : 1\ndecden - chara çevirmek için : 2")
-secim = input("Seçim : ")
-if(secim == "1"):
-    ilk_deger = input("1.değeri giriniz : ")
-    ikinci_deger = input("2.değeri giriniz, çevirmek için x : ")
-    if(ikinci_deger == "x"):
-        cevirici(ilk_deger)
-        print("Çıktı = {}".format(cevirici(ilk_deger)))
-        dosya = open("bilgiler.txt","w",encoding="utf-8")
-        dosya.write("{}".format(cevirici(ilk_deger)))
-        dosya.close()
-    else:
-        ucuncu_deger = input("3.değeri giriniz, çevirmek için x : ")
-        if(ucuncu_deger == "x"):
-            cevirici(ilk_deger)
-            A = ("{}".format(cevirici(cıktı)))
-            cevirici(ikinci_deger)
-            B = ("{}".format(cevirici(cıktı)))
-            print("Çıktı = " + A + B)
-            dosya = open("bilgiler.txt","w",encoding="utf-8")
-            dosya.write("Çıktı" + A + B)
-            dosya.close()
+dosya = open("bilgiler.txt","w",encoding="utf-8")
+values = []
+çıktı = []
+def Lister(Cdeger):
+    for i in range(0,lenDeger):
+        values.append(Cdeger[i])
+def cevirici():
+    for i in range(0,lenDeger):
+        if(values[i] == 'a'):
+            çıktı.append("97")
+        elif(values[i] == "b"):
+            çıktı.append("98")
+        elif(values[i] == "c"):
+            çıktı.append("99")
+        elif(values[i] == "d"):
+            çıktı.append("100")
+        elif(values[i] == "e"):
+            çıktı.append("101")
+        elif(values[i] == "f"):
+            çıktı.append("102")
+        elif(values[i] == "g"):
+            çıktı.append("103")
+        elif(values[i] == "h"):
+            çıktı.append("104")
+        elif(values[i] == "i"):
+            çıktı.append("105")
+        elif(values[i] == "j"):
+            çıktı.append("106")
+        elif(values[i] == "k"):
+            çıktı.append("107")
+        elif(values[i] == "l"):
+            çıktı.append("108")
+        elif(values[i] == "m"):
+            çıktı.append("109")
+        elif(values[i] == "n"):
+            çıktı.append("110")
+        elif(values[i] == "o"):
+            çıktı.append("111")
+        elif(values[i] == "p"):
+            çıktı.append("112")
+        elif(values[i] == "q"):
+            çıktı.append("113")
+        elif(values[i] == "r"):
+            çıktı.append("114")
+        elif(values[i] == "s"):
+            çıktı.append("115")
+        elif(values[i] == "t"):
+            çıktı.append("116")
+        elif(values[i] == "u"):
+            çıktı.append("117")
+        elif(values[i] == "v"):
+            çıktı.append("118")
+        elif(values[i] == "w"):
+            çıktı.append("119")
+        elif(values[i] == "x"):
+            çıktı.append("120")
+        elif(values[i] == "y"):
+            çıktı.append("121")
+        elif(values[i] == "z"):
+            çıktı.append("122")
+        elif(values[i] == "0"):
+            çıktı.append("48")
+        elif(values[i] == "1"):
+            çıktı.append("49")
+        elif(values[i] == "2"):
+            çıktı.append("50")
+        elif(values[i] == "3"):
+            çıktı.append("51")
+        elif(values[i] == "4"):
+            çıktı.append("52")
+        elif(values[i] == "5"):
+            çıktı.append("53")
+        elif(values[i] == "6"):
+            çıktı.append("54")
+        elif(values[i] == "7"):
+            çıktı.append("55")
+        elif(values[i] == "8"):
+            çıktı.append("56")
+        elif(values[i] == "9"):
+            çıktı.append("57")
         else:
-            dorduncu_deger = input("4.değeri giriniz, çevirmek için x : ")
-            if(dorduncu_deger == "x"):
-                cevirici(ilk_deger)
-                A = ("{}".format(cevirici(cıktı)))
-                cevirici(ikinci_deger)
-                B = ("{}".format(cevirici(cıktı)))
-                cevirici(ucuncu_deger)
-                C = ("{}".format(cevirici(cıktı)))
-                print("Çıktı = " + A + B + C)
-                dosya = open("bilgiler.txt","w",encoding="utf-8")
-                dosya.write("Çıktı" + A + B + C)
-                dosya.close()
-            else:
-                besinci_deger = input("5.değeri giriniz, çevirmek için x : ")
-                if(besinci_deger == "x"):
-                    cevirici(ilk_deger)
-                    A = ("{}".format(cevirici(cıktı)))
-                    cevirici(ikinci_deger)
-                    B = ("{}".format(cevirici(cıktı)))
-                    cevirici(ucuncu_deger)
-                    C = ("{}".format(cevirici(cıktı)))
-                    cevirici(dorduncu_deger)
-                    D = ("{}".format(cevirici(cıktı)))                
-                    print("Çıktı = " + A + B + C + D)
-                    dosya = open("bilgiler.txt","w",encoding="utf-8")
-                    dosya.write("Çıktı" + A + B + C + D)
-                    dosya.close()
-                else:
-                    altinci_deger = input("6.değeri giriniz, çevirmek için x : ")
-                    if(altinci_deger == "x"):
-                        cevirici(ilk_deger)
-                        A = ("{}".format(cevirici(cıktı)))
-                        cevirici(ikinci_deger)
-                        B = ("{}".format(cevirici(cıktı)))
-                        cevirici(ucuncu_deger)
-                        C = ("{}".format(cevirici(cıktı)))
-                        cevirici(dorduncu_deger)
-                        D = ("{}".format(cevirici(cıktı)))
-                        cevirici(besinci_deger)
-                        E = ("{}".format(cevirici(cıktı)))
-                        print("Çıktı = " + A + B + C + D + E)
-                        dosya = open("bilgiler.txt","w",encoding="utf-8")
-                        dosya.write("Çıktı = " + A + B + C + D + E)
-                        dosya.close()
-                    else:
-                        yedinci_deger = input("çevirmek için x : ")
-                        if(yedinci_deger == "x"):
-                            cevirici(ilk_deger)
-                            A = ("{}".format(cevirici(cıktı)))
-                            cevirici(ikinci_deger)
-                            B = ("{}".format(cevirici(cıktı)))
-                            cevirici(ucuncu_deger)
-                            C = ("{}".format(cevirici(cıktı)))
-                            cevirici(dorduncu_deger)
-                            D = ("{}".format(cevirici(cıktı)))
-                            cevirici(besinci_deger)
-                            E = ("{}".format(cevirici(cıktı)))
-                            cevirici(altinci_deger)
-                            F = ("{}".format(cevirici(cıktı)))
-                            print("Çıktı = " + A + B + C + D + E + F)
-                            dosya = open("bilgiler.txt","w",encoding="utf-8")
-                            dosya.write("Çıktı" + A + B + C + D + E + F)
-                            dosya.close()
+            print("Lütfen geçerli bir değer seçiniz")
+            i -= 1
+MainScreen = 0
+while MainScreen == 0: 
+    print("------------------------\nASCII Dec-Char converter\n------------------------\nSonuçlar bilgiler.txt ye kaydedilecek\n------------------------")
+    secim = input("1.Char - Dec :\nSeçim : ")
+    if(secim == "1"):
+        Cdeger = input("Çevirilecek değer:")
+        lenDeger = len(Cdeger)
+        Lister(Cdeger)
+        cevirici()
+        toDeger = çıktı[0]
+        for i in range(1,lenDeger):
+            toplam = toDeger + çıktı[i]
+            toDeger = toplam
+        print("Sonuç: " + toDeger)
+        dosya.write(toDeger + "\n")
+        a = input("Çıkmak için x\nDevam etmek için a\n")
+        if a == "x":
+            MainScreen = 1
+    else:
+        print("Lütfen bir seçenek seçiniz")
+        input("")
+dosya.close()
